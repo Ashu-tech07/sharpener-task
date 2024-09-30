2a188c8c16b85e97a23cf484b56ae805f34c3b7d
Ashish Yadav <aybhu10@gmail.com>
2024-09-30 18:09:31 +0530

    creating reusable component

--- learning-react/src/App.js
+++ learning-react/src/App.js
@@ -1,35 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-import ExpenseItem from './components/ExpenseItem';
+
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    { id: "1", date: new Date(2023, 7, 15), title: "Insurance", price: 100, location:"Bangalore"},
+    { id: "2", date: new Date(2023, 3, 25), title: "Book", price: 10, location:"Delhi" },
+    { id: "3", date: new Date(2023, 10, 11), title: "Pen", price: 1, location:"Hyderabad" },
+    { id: "4", date: new Date(2023, 1, 14), title: "Laptop", price: 200, location:"Mumbai" },
+  ];
+
   return (
     <div>
       <h1>Let's get Started</h1>
-      <ExpenseItem
-        date={new Date(2023, 7, 15)}
-        title="Insurance"
-        price="50"
-        location="Bangalore"
-      ></ExpenseItem>
-      <ExpenseItem
-        date={new Date(2023, 3, 25)}
-        title="Book"
-        price="20"
-        location="Delhi"
-      ></ExpenseItem>
-      <ExpenseItem
-        date={new Date(2023, 10, 11)}
-        title="Pen"
-        price="5"
-        location="Hydrabad"
-      ></ExpenseItem>
-      <ExpenseItem
-        date={new Date(2023, 1, 14)}
-        title="Laptop"
-        price="200"
-        location="Mumbai"
-      ></ExpenseItem>
+      {expenses.map((expense, index) => {
+        return (
+          <ExpenseItem
+            key={expense.id}
+            date={expense.date}
+            location={expense.location}
+            title={expense.title}
+            price={expense.price}
+          ></ExpenseItem>
+        );
+      })}
     </div>
   );
 }
