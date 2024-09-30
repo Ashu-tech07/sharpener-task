462258dff91fdcdc94ff574696b311b486aaaeb4
Ashish Yadav <aybhu10@gmail.com>
2024-09-30 17:32:10 +0530

    passing props to child component

--- learning-react/src/App.js
+++ learning-react/src/App.js
@@ -4,9 +4,32 @@ import ExpenseItem from './components/ExpenseItem';
 
 function App() {
   return (
-    <div className="App">
-     <h1>Let's get started</h1>
-     <ExpenseItem />
+    <div>
+      <h1>Let's get Started</h1>
+      <ExpenseItem
+        date={new Date(2023, 7, 15)}
+        title="Insurance"
+        price="50"
+        location="Bangalore"
+      ></ExpenseItem>
+      <ExpenseItem
+        date={new Date(2023, 3, 25)}
+        title="Book"
+        price="20"
+        location="Delhi"
+      ></ExpenseItem>
+      <ExpenseItem
+        date={new Date(2023, 10, 11)}
+        title="Pen"
+        price="5"
+        location="Hydrabad"
+      ></ExpenseItem>
+      <ExpenseItem
+        date={new Date(2023, 1, 14)}
+        title="Laptop"
+        price="200"
+        location="Mumbai"
+      ></ExpenseItem>
     </div>
   );
 }
