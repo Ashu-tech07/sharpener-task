5717823921185211eee49bd4d8fd09c44300e5bc
Ashish Yadav <aybhu10@gmail.com>
2024-09-30 16:44:52 +0530

    add css

--- learning-react/src/components/ExpenseItem.js
+++ learning-react/src/components/ExpenseItem.js
@@ -1,15 +1,19 @@
-import React from 'react'
 
-const ExpenseItem = () => {
-    return (
-        <div>
-            <p>Food - $5</p>
+import "./ExpenseItem.css";
 
-           <p>Petrol - $20</p> 
-
-           <p>Movie - $10</p> 
+function ExpenseItem() {
+  return (
+    <div className='expense-item'>
+      <div>15 August 2023</div>
+      <div className='expense-item__description'>
+        <h2 >Book</h2>
+        <div className='expense-item__price'>$10</div>
+        <div className='expense-item__location'>Delhi
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default ExpenseItem
+export default ExpenseItem;
+
