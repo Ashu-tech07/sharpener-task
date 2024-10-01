b7685f0988936de4f019c3523406e52c3f92b3aa
Ashish Yadav <aybhu10@gmail.com>
2024-10-01 12:26:39 +0530

    listening for events

--- learning-react/src/components/ExpenseItem.js
+++ learning-react/src/components/ExpenseItem.js
@@ -1,17 +1,24 @@
+import { useState } from "react";
 import Card from "./Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 
 function ExpenseItem(props) {
+  const [title, setTitle]=useState(props.title);
+
+  function buttonClickHandler(event){
+    setTitle("New Title");
+  }
   
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}/>
       <div className="expense-item__location">{props.location}</div>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.price}</div>
+        <button onClick={buttonClickHandler}>Change Title</button>
       </div>
     </Card>
   );
