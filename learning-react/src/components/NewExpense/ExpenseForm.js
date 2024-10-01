2fa1649c7c8ffcf525b21de4fb8bc50e9239a848
Ashish Yadav <aybhu10@gmail.com>
2024-10-01 22:18:53 +0530

    render expense form conditionally

--- learning-react/src/components/NewExpense/ExpenseForm.js
+++ learning-react/src/components/NewExpense/ExpenseForm.js
@@ -1,3 +1,5 @@
+// Write your code at relevant places in the code below:
+
 import React, { useState } from "react";
 import "./ExpenseForm.css";
 
@@ -5,49 +7,89 @@ const ExpenseForm = (props) => {
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
+  const [isOpenForm, setIsOpenForm]=useState(false);
 
   const titleChangeHandler = (event) => {
     setEnteredTitle(event.target.value);
   };
+
   const amountChangeHandler = (event) => {
     setEnteredAmount(event.target.value);
   };
+
   const dateChangeHandler = (event) => {
     setEnteredDate(event.target.value);
   };
 
-  const handleFormSubmit=(event)=>{
-    const expenseData={
-        title:enteredTitle,
-        amount:enteredAmount,
-        date: new Date(enteredDate)
-    }
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+    const expenseData = {
+      title: enteredTitle,
+      price: enteredAmount,
+      date: new Date(enteredDate),
+    };
     props.onSaveExpenseData(expenseData);
-    setEnteredAmount('');
-    setEnteredTitle('');
-    setEnteredDate('');
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
+    setIsOpenForm(false);
+  };
+
+  const handleToggleButton=()=>{
+    setIsOpenForm(true);
   }
 
+  const handleCloseButton=()=>{
+    setIsOpenForm(false);
+  }
+  
   return (
-    <form onSubmit={handleFormSubmit}>
+    <div>
+    { isOpenForm ? (
+      <form onSubmit={formSubmitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label htmlFor="title">Title</label>
-          <input type="text" id="title" value={enteredTitle} onChange={titleChangeHandler} />
+          <input
+            type="text"
+            value={enteredTitle}
+            id="title"
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label htmlFor="amount">Amount</label>
-          <input type="number" id="amount" value={enteredAmount} onChange={amountChangeHandler}/>
+          <input
+            type="number"
+            value={enteredAmount}
+            id="amount"
+            onChange={amountChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label htmlFor="date">Date</label>
-          <input type="date" id="date" min="2023-01-01" max="2024-12-31" value={enteredDate} onChange={dateChangeHandler}/>
+          <input
+            type="date"
+            value={enteredDate}
+            id="date"
+            min="2023-01-01"
+            max="2024-12-31"
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
+        <button onClick={handleCloseButton}>Close</button>
         <button type="submit">Add Expense</button>
       </div>
     </form>
+    ) : (<div className="new-expense__controls">
+      <div className="new-expense__actions__close">
+        <button onClick={handleToggleButton}>Add Expense</button>
+      </div>
+    </div>)
+} 
+</div>
   );
 };
 
