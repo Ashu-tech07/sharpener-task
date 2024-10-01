03e4075131bafb31505b97fb09508b6269596def
Ashish Yadav <aybhu10@gmail.com>
2024-10-01 17:39:44 +0530

    expenses filter by year

--- learning-react/src/components/Expenses/Expenses.js
+++ learning-react/src/components/Expenses/Expenses.js
@@ -2,12 +2,22 @@ import React from 'react'
 import ExpenseItem from './ExpenseItem';
 import './Expenses.css'
 import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
 
 const Expense = (props) => {
-    const expenses=props.expenses
+    const expenses=props.expenses;
+    const [filteredYear,setFilteredYear]=useState("2023");
+
+    const filteredExpenses=expenses.filter((item)=>item.date.getFullYear().toString()===filteredYear)
+    
+
+    const changeFilterHandler=(selectedYear)=>{
+      setFilteredYear(selectedYear);
+    }
   return (
     <Card className='expenses'>
-       {expenses.map((expense) => {
+      <ExpensesFilter selected={filteredYear} onChangeFilter={changeFilterHandler}/>
+       {filteredExpenses.map((expense) => {
         return (
           <ExpenseItem
             key={expense.id}
