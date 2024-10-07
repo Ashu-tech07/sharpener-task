b5a86a49cba049783572e025e2a6a2933397d3f3
Ashish Yadav <aybhu10@gmail.com>
2024-10-07 21:13:27 +0530

    showing and enabling the delete of Cart Items

--- restaurants-website/src/components/Cart/Cart.js
+++ restaurants-website/src/components/Cart/Cart.js
@@ -6,6 +6,21 @@ import CartContext from '../../store/cartContext'
 const Cart = (props) => {
     const ctx = useContext(CartContext)
 
+    const increaseItem = (itemId) => {
+        ctx.items.forEach((item) => {
+            if (item.id === itemId) {
+                ctx.addItem({ ...item, quantity: 1 });
+            }
+        });
+    };
+    const decreaseItem = (itemId) => {
+        ctx.items.forEach((item) => {
+            if (item.id === itemId) {
+                ctx.removeItem(itemId);
+            }
+        });
+    };
+
     let totalPrice = 0;
     ctx.items.forEach((item) => {
         totalPrice += Number(item.price) * Number(item.quantity);
@@ -17,9 +32,23 @@ const Cart = (props) => {
                 <ul className={classes["cart-items"]}>
                     {ctx.items.map((item) => (
                         <li key={item.id}>
-                            <span> Name: </span> {item.name}
-                            <span> Quantity: </span> {item.quantity}
-                            <span> Price: </span> ${item.price}
+                            <span>{item.name}</span>
+                            <span>x{item.quantity}</span>
+                            <span>${item.price}</span>
+                            <div className="btnGroup">
+                                <button
+                                    className={classes["button--decrease"]}
+                                    onClick={() => decreaseItem(item.id)}
+                                >
+                                    -
+                                </button>
+                                <button
+                                    className={classes["button--increase"]}
+                                    onClick={() => increaseItem(item.id)}
+                                >
+                                    +
+                                </button>
+                            </div>
                         </li>
                     ))}
                 </ul>
