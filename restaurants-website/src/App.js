560aad2251078a65469b2740181dc38e71f00ea6
Ashish Yadav <aybhu10@gmail.com>
2024-10-07 16:15:31 +0530

    adding header component

---
+++ restaurants-website/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Header from './components/Layout/Header';
+
+function App() {
+  return (
+    <>
+    <Header/>
+    </>
+  );
+}
+
+export default App;
---
+++ restaurants-website/src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import mealsImage from '../assets/meals.jpg'
+import HeaderCartButton from './HeaderCartButton';
+import classes from './Header.module.css';
+
+const Header = () => {
+    return (
+        <>
+            <header className={classes.header}>
+                <h1>ReactMeals</h1>
+                <HeaderCartButton />
+            </header>
+            <div className={classes["main-image"]}>
+                <img src={mealsImage} alt="meals image" />
+            </div>
+        </>
+    )
+}
+
+export default Header
