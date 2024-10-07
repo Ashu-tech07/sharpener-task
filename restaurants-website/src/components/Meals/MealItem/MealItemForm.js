7f23b93558eeec1b72e8085e45f51fb1c9300d9f
Ashish Yadav <aybhu10@gmail.com>
2024-10-07 17:44:24 +0530

    added input form

--- restaurants-website/src/components/Meals/MealItem/MealItemForm.js
+++ restaurants-website/src/components/Meals/MealItem/MealItemForm.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import classes from './MealItemForm.module.css'
+import Input from '../../UI/Input'
 
-const MealItemForm = () => {
+const MealItemForm = (props) => {
   return (
-    <div>
-      
-    </div>
+    <form className={classes.form}>
+      <Input
+        label="Amount"
+        input={{
+          id: "amount",
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
+        }}
+      />
+      <button>+ Add</button>
+    </form>
   )
 }
 
