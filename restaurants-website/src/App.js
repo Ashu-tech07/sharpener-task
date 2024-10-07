aab4315ff0c54406f8dbdb3ac0c8eda1335204f1
Ashish Yadav <aybhu10@gmail.com>
2024-10-07 18:13:01 +0530

    added hardcoded CART

--- restaurants-website/src/App.js
+++ restaurants-website/src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import Cart from './components/Cart/Cart';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 
 function App() {
   return (
     <>
+    <Cart />
     <Header/>
     <main>
       <Meals/>
---
+++ restaurants-website/src/components/Cart/Cart.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import classes from './Cart.module.css'
+import Modal from '../UI/Modal'
+const Cart = (props) => {
+    return (
+        <Modal>
+            {
+                <ul className={classes["car-items"]}>
+                    {[{ id: "c1", name: "Shushi", amount: 2, price: 12.99 }].map((item) => (
+                        <li>{item.name}</li>
+                    ))}
+                </ul>
+            }
+            <div className={classes.total}>
+                <span>Total Amount</span>
+                <span>35.62</span>
+            </div>
+            <div className={classes.actions}>
+                <button className={classes["buttton--alt"]}>close</button>
+                <button className={classes.buttom}>Order</button>
+            </div>
+        </Modal>
+    )
+}
+
+export default Cart
