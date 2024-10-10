71adf1479d952145ef82ed724268f6bd057d5c3d
Ashish Yadav <aybhu10@gmail.com>
2024-10-10 16:54:07 +0530

    added Cart Component

--- e-commerce-website/src/components/Header.js
+++ e-commerce-website/src/components/Header.js
@@ -1,7 +1,13 @@
-import React from 'react';
-import { Container, Navbar, Nav } from "react-bootstrap";
+import React, { useState } from 'react';
+import { Container, Navbar, Nav, Button } from "react-bootstrap";
+import Cart from './Cart/Cart';
 
 const Header = () => {
+  const [showCart, setShowCart] = useState(false);
+  const showCartHandler = ()=>{
+    setShowCart(!showCart)
+  }
+
   return (
     <header>
       <Navbar expand="lg" className="bg-body-tertiary">
@@ -16,8 +22,10 @@ const Header = () => {
             </Nav>
           </Navbar.Collapse>
         </Container>
+        <Button onClick={showCartHandler}>Cart</Button>
       </Navbar>
-    </header>
+      {showCart && <Cart showCartHandler={showCartHandler}></Cart>}
+      </header>
   );
 };
 export default Header;
