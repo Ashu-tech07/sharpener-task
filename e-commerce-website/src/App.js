953fc434edfedca44c0486082064cb490f6e20f9
Ashish Yadav <aybhu10@gmail.com>
2024-10-10 16:34:58 +0530

    Building Blocks of Ecommerce

---
+++ e-commerce-website/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Header from './components/Header';
+import ProductList from './components/ProductList';
+
+function App() {
+  return (
+   <>
+   <Header/>
+   <ProductList/>
+   
+   </>
+  );
+}
+
+export default App;
