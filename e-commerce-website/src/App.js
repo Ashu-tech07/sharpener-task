08c9333ca690a533b7f0b36a81383f47066e8afd
Ashish Yadav <aybhu10@gmail.com>
2024-10-11 20:16:09 +0530

    added new movie into the list

--- e-commerce-website/src/App.js
+++ e-commerce-website/src/App.js
@@ -18,7 +18,8 @@
 // }
 // export default App;
 
-import React, { useCallback, useEffect, useMemo, useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
+import AddMovie from "./components/movie/AddMovie";
 import MoviesList from "./components/movie/MoviesList";
 import "./App.css";
 
@@ -26,17 +27,15 @@ function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsloading] = useState(false);
   const [error, setError] = useState(null);
-  const [retryCount, setRetryCount] = useState(0);
-  const [retryIntervalId, setRetryIntervalId] = useState(null);
- 
- 
+
+
   const fetchMoviesHandler = useCallback(async () => {
     setIsloading(true);
     setError(null);
     try {
       const response = await fetch("https://swapi.dev/api/films");
       if (!response.ok) {
-        throw new Error("Something went wrong ....Retrying");
+        throw new Error("Something went wrong!");
       }
       const data = await response.json();
       const transformedMovies = data.results.map((movieData) => {
@@ -48,61 +47,47 @@ function App() {
         };
       });
       setMovies(transformedMovies);
-      setRetryCount(0);
     } catch (error) {
       setError(error.message);
-      setRetryCount(retryCount + 1);
     }
     setIsloading(false);
-  },[])
-  
-  const cancleRetryHandler= useCallback(()=> {
-    clearInterval(retryIntervalId);
-    setRetryCount(0);
-  },[retryIntervalId])
+  }, []);
 
-  useEffect(() => {
-    if (retryCount > 0) {
-      setRetryIntervalId(
-        setInterval(() => {
-          fetchMoviesHandler();
-        }, 5000)
-      );
-    }
-    return () => {
-      clearInterval(retryIntervalId);
-    };
-  }, [retryCount,fetchMoviesHandler]);
 
   useEffect(() => {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
 
-  const content = useMemo(() => {
-    if (movies.length > 0) {
-      return <MoviesList movies={movies} />;
-    } else if (error) {
-      return (
-        <>
-          <p>{error}</p>
-          <button onClick={fetchMoviesHandler}>Retry</button>
-          <button onClick={cancleRetryHandler}>Cancel</button>
-        </>
-      );
-    } else if (isLoading) {
-      return <p>Loading....</p>;
-    } else {
-      return <p>Found No Movies.</p>;
-    }
-  }, [movies, error, isLoading, fetchMoviesHandler, cancleRetryHandler]);
+  function addMovieHandler(movie) {
+    console.log(movie);
+  }
 
+
+  let content = <p>Found no movies.</p>;
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
 
   return (
     <>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
       <section>{content}</section>
     </>
   );
 }
-
 export default App;
