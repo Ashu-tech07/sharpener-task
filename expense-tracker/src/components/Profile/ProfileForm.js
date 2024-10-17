acabde6402d2a176461e44594c45c87dca4292a9
Ashish Yadav <aybhu10@gmail.com>
2024-10-17 20:51:46 +0530

    updated Complete your profile page

--- expense-tracker/src/components/Profile/ProfileForm.js
+++ expense-tracker/src/components/Profile/ProfileForm.js
@@ -1,50 +1,81 @@
 import { useRef, useContext } from "react";
 import AuthContext from "../../Store/AuthContext";
 import classes from "./ProfileForm.module.css";
+import { useNavigate } from "react-router-dom";
 
 const ProfileForm = () => {
-  const newPasswordInputRef = useRef();
+
+  const fullNameInputRef = useRef();
+  const profileUrlInputRef = useRef();
 
   const authCtx = useContext(AuthContext);
+  const navigate = useNavigate();
 
-  const submitHandler = (event) => {
+  const cancelHandler = () => {
+    navigate("/");
+  };
+
+  const updateProfile = (event) => {
     event.preventDefault();
 
-    const enteredNewPassword = newPasswordInputRef.current.value;
-    
+    const enteredName = fullNameInputRef.current.value;
+    const enteredPhotoURL = profileUrlInputRef.current.value;
+
     fetch(
       "https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyA_8kdenpIeMebxIwDa8v3ED34bNDPgy5Y",
       {
         method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
         body: JSON.stringify({
           idToken: authCtx.token,
-          password: enteredNewPassword,
-          returnSecureToken: false,
+          displayName: enteredName,
+          photoUrl: enteredPhotoURL,
+          returnSecureToken: true,
         }),
-        headers: {
-          "Content-Type:": "application/json",
-        },
       }
-    ).then((res) => {
-      //assumption: Always succeeds
-    });
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        //Handle the successful response here
+        console.log(data);
+      })
+      .catch((error) => {
+        // Handle any errors here
+        console.error(error);
+      });
+    fullNameInputRef.current.value = "";
+    profileUrlInputRef.current.value = "";
   };
 
+
   return (
-    <form className={classes.form} onSubmit={submitHandler}>
-      <div className={classes.control}>
-        <label htmlFor="new-password">New Password</label>
-        <input
-          type="password"
-          id="new-password"
-          minLength="7"
-          ref={newPasswordInputRef}
-        />
-      </div>
-      <div className={classes.action}>
-        <button>Change Password</button>
-      </div>
-    </form>
+    <div>
+      <form className={classes.form}>
+        <div className={classes.control}>
+          <label htmlFor="full-name">Full Name</label>
+          <input type="text" id="full-name" ref={fullNameInputRef} required />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="photot-url">Profile Photot URL</label>
+          <input
+            type="text"
+            id="photot-url"
+            ref={profileUrlInputRef}
+            required
+          />
+        </div>
+        <div className={classes.action}>
+          <button onClick={updateProfile} className={classes.update}>
+            Update
+          </button>
+        </div>
+      </form>
+      <button onClick={cancelHandler} className={classes.cancel}>
+        Cancel
+      </button>
+    </div>
   );
 };
 export default ProfileForm;
--- expense-tracker/src/components/Profile/UserProfile.js
+++ expense-tracker/src/components/Profile/UserProfile.js
@@ -4,8 +4,8 @@ import classes from "./UserProfile.module.css";
 const UserProfile = () => {
     
   return (
-    <section className={classes.Profile}>
-      <h1>Your User Profile</h1>
+    <section >
+      <h1 className={classes.profile}>Contact Details</h1>
       <ProfileForm />
     </section>
   );
